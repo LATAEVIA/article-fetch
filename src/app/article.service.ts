dab5a0f2ead0457e4e871f1c0f821edf801d2b86
LaTaevia Berry <lataevia.berry@gmail.com>
2017-09-07 19:29:23 -0500

    commit working service

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,39 +1,38 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+// import {Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 
 export class ArticleService {
-//   constructor(private http:Http) {
-//
-//   }
-//   private url:string = "https://submissions.scholasticahq.com/api/v1/articles.json?per_page=10";
-//   getArticles() {
-//     return this.http.get(this.url).map((response:Response) => response.json());
-//   }
-// }
-
-
-// data: Object;
-//
-// constructor(public http: Http) {
-// }
-// getArticles() {
-//     return this.http.get('https://submissions.scholasticahq.com/api/v1/articles.json?per_page=10')
-//     .map((res: Response) => {
-//       this.data = res.json()['articles'];
-//     });
-//   }
-
   data: Object;
+
   constructor(private http:Http) {
 
+
   }
-  private url:string = "https://submissions.scholasticahq.com/api/v1/articles.json?per_page=10";
+  private someArticlesUrl:string = "https://submissions.scholasticahq.com/api/v1/articles.json?per_page=10";
   getArticles() {
-    return this.http.get(this.url).map((response:Response) => this.data = response.json()['articles']);
+    return this.http.get(this.someArticlesUrl).map((response:Response) => this.data = response.json()['articles']);
+    // return this.http
+    // .get(this.someArticlesUrl)
+    // .subscribe(resp => {console.log(resp)});
   }
 
+  private moreArticlesUrl:string = "https://submissions.scholasticahq.com/api/v1/articles.json?per_page=10&offset=10";
+  getMoreArticles() {
+    // return this.http.get(this.moreArticlesUrl).map((response:Response) => this.data = response.json()['articles']);
+    return this.http
+    .get(this.moreArticlesUrl)
+    .map((response:Response) => this.data = response.json()['articles'])
+    // .push(articles);
+
+    // return this.http
+    // .get(this.moreArticlesUrl)
+    // .subscribe(resp => {console.log(resp)});
+
+  }
 }
