21a9be711a634379341fe46a2944b8818a462952
LaTaevia Berry <lataevia.berry@gmail.com>
2017-09-08 14:48:10 -0500

    commit working two user story app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,18 @@ import { SlicePipe } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: []
 })
+
 export class AppComponent implements OnInit {
-  // title = 'Article Fetch';
   constructor(private articleService:ArticleService, private router: Router) {}
 
   limit = 10;
   articles = [];
   singleArticles: any[]=null;
-  // albums: Album[] = [
-  //  ...
-  // ];
-  // moreArticles = [];
+
   ngOnInit(){
     this.articleService.getArticles().subscribe(responseArticles => this.articles = responseArticles);
-    // this.articleService.getMoreArticles().subscribe(responseArticles => this.articles = responseArticles);
   }
+  
   getSingleAtricle(id: string) {
     this.articleService.getAtricleById(id).subscribe(response => {
         this.singleArticles = response.json();
